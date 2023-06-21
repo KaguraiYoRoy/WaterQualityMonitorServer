b9a4073e710be773023a791506802ac569890d3d
iYoRoy <3363384229@qq.com>
2023-06-22 00:46:50 +0800

    Admin: Print data

--- admin/index.php
+++ admin/index.php
@@ -22,9 +22,10 @@ if(!$query_res){
 	die($errmsg[4]['msg']);
 }
 if(!$row = mysqli_fetch_array($query_res)){
-	echo "Login Failed!";
 	header('Refresh:3;url=login.html');
+	die("Login Failed!");
 }
+$id = $row['id'];
 
 ?>
 <html>
@@ -33,12 +34,65 @@ if(!$row = mysqli_fetch_array($query_res)){
 	<title>WaterMonitor Login</title>
 </head>
 <body>
+<hr>
 <?php
-	
+
+$sql = "select count(*) from data where id = $id";
+$query_res = query_sql($sql);
+if(!$query_res){
+	die($errmsg[4]['msg']);
+}
+$datarows = 0;
+if($row = mysqli_fetch_array($query_res)){
+	$datarows = $row[0];
+}
+
+echo "Total: $datarows rows.<br>";
+
+$pages = ($datarows - 1) /50 + 1;
+$cur_page = isset($_REQUEST['page'])?$_REQUEST['page']:1;
+
+$start = 50 * ($cur_page - 1);
+
+$sql = "select * from data where id=$id limit $start,50";
+$query_res = query_sql($sql);
+if(!$query_res){
+	die($errmsg[4]['msg']);
+}
+
 ?>
-	<form action="login.php?logout=1" method="post">
-		<input type="submit" value="注销">
-	</form>
+
+<table>
+	<tr>
+		<th>Time</th>
+		<th>Core Temp</th>
+		<th>Water Temp</th>
+		<th>TDS</th>
+		<th>PH</th>
+		<th>Turbidity</th>
+	</tr>
+
+<?php
+
+while($row = mysqli_fetch_array($query_res)){
+
+	echo "<tr>";
+	echo "<th>" . $row['time'] . "</th>";
+	echo "<th>" . $row['lm35'] . "</th>";
+	echo "<th>" . $row['watertemp'] . "</th>";
+	echo "<th>" . $row['tds'] . "</th>";
+	echo "<th>" . $row['ph'] . "</th>";
+	echo "<th>" . $row['turbidity'] . "</th>";
+	echo "</tr>";
+
+}
+
+?>
+</table>
+<hr>
+<form action="login.php?logout=1" method="post">
+	<input type="submit" value="注销">
+</form>
 </body>
 <footer>
 	<p>Copyright. ©2020-2023 iYoRoy Studio.
