578dc7bc46122bb3273ce38f779fff9404ef074b
KaguraiYoRoy <3363384229@qq.com>
2023-07-28 10:47:27 +0800

    API: close mysql connection when scheduled task is done

--- api/schedule.php
+++ api/schedule.php
@@ -12,6 +12,15 @@ if(!$conn){//连接数据库
 
 $sql = "update tokens set online=0 where DATE_ADD(lastrequest,INTERVAL 1 MINUTE)<".date('Y-m-d H:i:s');
 
+echo json_encode(array("result"=>0,"msg"=>"OK"));
+
+cleanup();
+
+function cleanup(){
+	global $conn;
+	mysqli_close($conn);
+}
+
 function query_sql($sql){
 	global $conn;
 	$query_res = mysqli_query($conn,$sql);
