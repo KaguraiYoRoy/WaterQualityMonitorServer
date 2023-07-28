ae7de4d597eea1b16ffcd7c32c299eecdd615367
KaguraiYoRoy <3363384229@qq.com>
2023-07-28 10:32:38 +0800

    API: add schedule script

---
+++ api/schedule.php
@@ -0,0 +1,21 @@
+<?php
+require '../config.php';
+
+if(!$service_status){ //总开关，定义在config.php
+	die($errmsg[1]['msg']);
+}
+
+$conn = mysqli_connect($mysql_server_host, $mysql_username, $mysql_password, $mysql_database);
+if(!$conn){//连接数据库
+	die($errmsg[2]['msg']);
+}
+
+$sql = "update tokens set online=0 where DATE_ADD(lastrequest,INTERVAL 1 MINUTE)<".date('Y-m-d H:i:s');
+
+function query_sql($sql){
+	global $conn;
+	$query_res = mysqli_query($conn,$sql);
+	return $query_res;
+}
+
+?>
